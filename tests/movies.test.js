ba285636fd29686659b61d3200f9e95b02f9497e
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-30 16:58:31 +0100

    Learning POST routes testing...

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -13,6 +13,7 @@ describe("GET /api/movies", () => {
     const response = await request(app).get("/api/movies");
     expect(response.headers["content-type"]).toMatch(/json/);
   });
+
   it("should check if the response has a status code of 200", async () => {
     const response = await request(app).get("/api/movies");
     expect(response.status).toEqual(200);
@@ -24,12 +25,103 @@ describe("GET /api/movies/:id", () => {
     const response = await request(app).get("/api/movies/1");
     expect(response.headers["content-type"]).toMatch(/json/);
   });
+
   it("should check if the response has a status code of 200", async () => {
     const response = await request(app).get("/api/movies/1");
     expect(response.status).toEqual(200);
   });
+
   it("should return no movie", async () => {
     const response = await request(app).get("/api/movies/0");
     expect(response.status).toEqual(404);
+  });
+});
+
+describe("POST /api/movies", () => {
+  it("should check if the response has a json format", async () => {
+    const newMovie = {
+      title: "The Matrix",
+      director: "Lana Wachowski",
+      year: 1999,
+      color: "1",
+      duration: 136
+    };
+    const response = await request(app).post("/api/movies").send(newMovie);
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+
+  it("should check if the response has a status code of 201", async () => {
+    const newMovie = {
+      title: "The Matrix",
+      director: "Lana Wachowski",
+      year: "1999",
+      color: "1",
+      duration: 136
+    };
+    const response = await request(app).post("/api/movies").send(newMovie);
+    expect(response.status).toEqual(201);
+  });
+
+  it("should check the existence of all required fields", async () => {
+    const newMovie = {
+      title: "The Matrix",
+      director: "Lana Wachowski",
+      year: "1999",
+      color: "1",
+      duration: 136
+    };
+    const response = await request(app).post("/api/movies").send(newMovie);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("title");
+    expect(response.body).toHaveProperty("director");
+    expect(response.body).toHaveProperty("year");
+    expect(response.body).toHaveProperty("color");
+    expect(response.body).toHaveProperty("duration");
+
+    const [movie] = await database.query("SELECT * FROM `movies` WHERE `id` = ?", [response.body.id]);
+    const [movieFromDatabase] = movie;
+    expect(movieFromDatabase).toHaveProperty("id");
+    expect(movieFromDatabase).toHaveProperty("title");
+    expect(movieFromDatabase).toHaveProperty("director");
+    expect(movieFromDatabase).toHaveProperty("year");
+    expect(movieFromDatabase).toHaveProperty("color");
+    expect(movieFromDatabase).toHaveProperty("duration");
+  });
+
+  it("should check the type of all required fields", async () => {
+    const newMovie = {
+      title: "The Matrix",
+      director: "Lana Wachowski",
+      year: "1999",
+      color: "1",
+      duration: 136
+    };
+    const response = await request(app).post("/api/movies").send(newMovie);
+    expect(typeof response.body.id).toBe("number");
+    expect(typeof response.body.title).toBe("string");
+    expect(typeof response.body.director).toBe("string");
+    expect(typeof response.body.year).toBe("string");
+    expect(typeof response.body.color).toBe("string");
+    expect(typeof response.body.duration).toBe("number");
+
+    const [movie] = await database.query("SELECT * FROM `movies` WHERE `id` = ?", [response.body.id]);
+    const [movieFromDatabase] = movie;
+    expect(typeof movieFromDatabase.id).toBe("number");
+    expect(typeof movieFromDatabase.title).toBe("string");
+    expect(typeof movieFromDatabase.director).toBe("string");
+    expect(typeof movieFromDatabase.year).toBe("string");
+    expect(typeof movieFromDatabase.color).toBe("string");
+    expect(typeof movieFromDatabase.duration).toBe("number");
+  });
+
+  it("should return an error if a required field is missing", async () => {
+    const newMovie = {
+      title: "The Matrix",
+      director: "Lana Wachowski",
+      year: 1999,
+      duration: 136
+    };
+    const response = await request(app).post("/api/movies").send(newMovie);
+    expect(response.status).toEqual(400);
   });
 });
--- tests/users.test.js
+++ tests/users.test.js
@@ -3,6 +3,8 @@ const request = require("supertest");
 const app = require("../src/app");
 
 const database = require("../database");
+const crypto = require("node:crypto");
+const e = require("express");
 
 afterAll(async () => {
   await database.end();
@@ -13,6 +15,7 @@ describe("GET /api/users", () => {
     const response = await request(app).get("/api/users");
     expect(response.headers["content-type"]).toMatch(/json/);
   });
+
   it("should check if the response has a status code of 200", async () => {
     const response = await request(app).get("/api/users");
     expect(response.status).toEqual(200);
@@ -24,12 +27,115 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/1");
     expect(response.headers["content-type"]).toMatch(/json/);
   });
+
   it("should check if the response has a status code of 200", async () => {
     const response = await request(app).get("/api/users/1");
     expect(response.status).toEqual(200);
   });
+
   it("should return no user", async () => {
     const response = await request(app).get("/api/users/0");
     expect(response.status).toEqual(404);
+  });
+});
+
+describe("POST /api/users", () => {
+  it("should check if the response has a json format", async () => {
+    const newUser = {
+      firstname: "John",
+      lastname: "Doe",
+      email: `${crypto.randomUUID()}@example.com`,
+      city: "Paris",
+      language: "French"
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+
+  it("should check if the response has a status code of 201", async () => {
+    const newUser = {
+      firstname: "John",
+      lastname: "Doe",
+      email: `${crypto.randomUUID()}@example.com`,
+      city: "Paris",
+      language: "French"
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+    expect(response.status).toEqual(201);
+  });
+
+  it("should check the existence of all required fields", async () => {
+    const newUser = {
+      firstname: "John",
+      lastname: "Doe",
+      email: `${crypto.randomUUID()}@example.com`,
+      city: "Paris",
+      language: "French"
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("firstname");
+    expect(response.body).toHaveProperty("lastname");
+    expect(response.body).toHaveProperty("email");
+    expect(response.body).toHaveProperty("city");
+    expect(response.body).toHaveProperty("language");
+
+    const [user] = await database.query("SELECT * FROM `users` WHERE `id` = ?", [response.body.id]);
+    const [userFromDatabase] = user;
+    expect(userFromDatabase).toHaveProperty("id");
+    expect(userFromDatabase).toHaveProperty("firstname");
+    expect(userFromDatabase).toHaveProperty("lastname");
+    expect(userFromDatabase).toHaveProperty("email");
+    expect(userFromDatabase).toHaveProperty("city");
+    expect(userFromDatabase).toHaveProperty("language");
+  });
+
+  it("should check the type of all required fields", async () => {
+    const newUser = {
+      firstname: "John",
+      lastname: "Doe",
+      email: `${crypto.randomUUID()}@example.com`,
+      city: "Paris",
+      language: "French"
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+    expect(typeof response.body.id).toBe("number");
+    expect(typeof response.body.firstname).toBe("string");
+    expect(typeof response.body.lastname).toBe("string");
+    expect(typeof response.body.email).toBe("string");
+    expect(typeof response.body.city).toBe("string");
+    expect(typeof response.body.language).toBe("string");
+
+    const [user] = await database.query("SELECT * FROM `users` WHERE `id` = ?", [response.body.id]);
+    const [userFromDatabase] = user;
+    expect(typeof userFromDatabase.id).toBe("number");
+    expect(typeof userFromDatabase.firstname).toBe("string");
+    expect(typeof userFromDatabase.lastname).toBe("string");
+    expect(typeof userFromDatabase.email).toBe("string");
+    expect(typeof userFromDatabase.city).toBe("string");
+    expect(typeof userFromDatabase.language).toBe("string");
+  });
+
+  it("should return a 400 status code", async () => {
+    const newUser = {
+      firstname: "John",
+      lastname: "Doe",
+      email: "sdcvdf@dd.dd",
+      city: "Paris"
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+    expect(response.status).toEqual(400);
+  });
+
+  it("should return a 409 status code", async () => {
+    const newUser = {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        city: "Paris",
+        language: "English"
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+    expect(response.status).toEqual(409);
   });
 });
