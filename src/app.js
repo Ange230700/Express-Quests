c0568f339e60b4c6f9b90389e741eaf5f6ec012c
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-30 12:23:09 +0100

    ...

--- src/app.js
+++ src/app.js
@@ -6,20 +6,19 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
-const validateMovie = require("./middlewares/validateMovie");
-const validateUser = require("./middlewares/validateUser");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
+app.post("/api/movies", movieControllers.postMovie);
+
+app.put("/api/movies/:id", movieControllers.putMovie);
+
+
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 
-app.post("/api/movies", validateMovie, movieControllers.postMovie);
-
-app.post("/api/users", validateUser, userControllers.postUser);
-
-app.put("/api/movies/:id", movieControllers.putMovie);
+app.post("/api/users", userControllers.postUser);
 
 app.put("/api/users/:id", userControllers.putUser);
 
--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,28 +1,13 @@
-const e = require("express");
-const Joi = require("joi");
+const { body, validationResult } = require("express-validator");
 
-const userSchema = Joi.object({
-    firstname: Joi.string().max(255).required(),
-    lastname: Joi.string().max(255).required(),
-    email: Joi.string().email().max(255).required(),
-    city: Joi.string().max(255).required(),
-    language: Joi.string().max(255).required(),
-});
 
 const validateUser = (request, response, next) => {
-    const { firstname, lastname, email, city, language } = request.body;
-    const { error } = userSchema.validate(
-        { firstname, lastname, email, city, language },
-        { abortEarly: false }
-    );
-
-    if (error) {
-        response.status(422).json({ validationErrors: error.details });
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() });
     }
 
-    else {
-        next();
-    }
+    next();
 };
 
 module.exports = validateUser;
